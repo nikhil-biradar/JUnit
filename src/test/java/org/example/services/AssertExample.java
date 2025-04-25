7a34d59b959f173de80a09801a4f7713924aa25b
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-04-25 16:53:37 +0530

    modified assetExample

--- src/test/java/org/example/services/AssertExample.java
+++ src/test/java/org/example/services/AssertExample.java
@@ -1,28 +1,27 @@
 package org.example.services;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertExample {
     @Test
     public void test1(){
         String name = "Nikhil";
         String expectedName = new String("Nikhil").intern();
-
-        Assertions.assertSame(name, expectedName);
-        Assertions.assertEquals(name, expectedName);
+        assertSame(name, expectedName);
+        assertEquals(name, expectedName);
 
         boolean value = true;
-        Assertions.assertTrue(value);
+        assertTrue(value);
 
         boolean value1 = false;
-        Assertions.assertFalse(value1);
+        assertFalse(value1);
 
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
         List<Integer> list1 = Arrays.asList(1, 2, 3,4, 5);
-        Assertions.assertIterableEquals(list1, list);
+        assertIterableEquals(list1, list);
 
     }
 }
--- src/test/java/org/example/services/CalculatorServiceTestJUnit5.java
+++ src/test/java/org/example/services/CalculatorServiceTestJUnit5.java
@@ -1,6 +1,7 @@
 package org.example.services;
 
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorServiceTestJUnit5 {
 
@@ -18,25 +19,25 @@ public class CalculatorServiceTestJUnit5 {
     public void addTwoNumbers(){
         System.out.println("First Test Case");
         int result = CalculatorService.addTwoNumbers(10, 50);
-        Assertions.assertEquals(60, result, "result should be 60");
+        assertEquals(60, result, "result should be 60");
     }
 
     @Test
     public void productTwoNumbers(){
         int result = CalculatorService.productTwoNumbers(5, 5);
-        Assertions.assertEquals(25, result);
+        assertEquals(25, result);
     }
 
     @Test
     public void divideTwoNumbers(){
         int result = CalculatorService.divideTwoNumbers(50, 5);
-        Assertions.assertEquals(10, result);
+        assertEquals(10, result);
     }
 
     @Test
     public void addAnyNumbers(){
         int result = CalculatorService.sumAnyNumbers(10, 20, 30, 40, 50);
-        Assertions.assertEquals(150, result);
+        assertEquals(150, result);
     }
 
     @AfterEach
